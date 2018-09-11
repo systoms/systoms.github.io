ee6ad276d239acaef539e9a76cc6bf63d6859261
岁月神偷 <systom@sina.cn>
2018-09-11 16:42:25 +0800

    init

---
+++ annotation/App.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: systom
+ * Date: 2018/9/10
+ * Time: 17:10
+ */
+
+class App
+{
+
+}
---
+++ annotation/vendor/systoms/annotation/src/Entity.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: systom
+ * Date: 2018/9/10
+ * Time: 17:02
+ */
+
+namespace systoms\annotation;
+
+
+class Entity
+{
+
+}
